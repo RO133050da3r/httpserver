f9655842e7f411ea482641309f6f19065ae83730
Nickolas22 <Nicko_2680@hotmail.dk>
2014-10-02 12:48:16 +0200

    Logging færidg

--- httpserver/logging.cs
+++ httpserver/logging.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -10,12 +11,15 @@ namespace httpserver
 
 {
     class Logging
-    {
+    {        
+        // her erklære vi vores log og vores loggingkilde
         const string Source = "Vores http server";
         const string sLog = "Application";
         
+        // her opretter vi et event log
         public static void WriteInfo(string message)
         {
+            // her fortæller vi systemet at hvis kilden til vores log ikke eksistere, skal den oprette en
             if (!EventLog.SourceExists(Source))
             {
                 EventLog.CreateEventSource(Source, sLog);
@@ -23,13 +27,40 @@ namespace httpserver
             
             string machineName = "."; // this computer
             using (EventLog log = new EventLog(sLog, machineName, Source))
-            {
-               
+            {  
+                //system skriver og sender en event/besked til vores logbog
                 log.WriteEntry(message, EventLogEntryType.Information);
-                
             }
         }
 
+        // Det samme sker her, bare med en Error besked
+        public static void WriteError(string message)
+        {
+            if (!EventLog.SourceExists(Source))
+            {
+                EventLog.CreateEventSource(Source, sLog);
+            }
 
+            string machineName = "."; // this computer
+            using (EventLog log = new EventLog(sLog, machineName, Source))
+            {
+                log.WriteEntry(message, EventLogEntryType.Error);
+            }
+        }
+
+        // Det samme sker her, bare med en Warning besked
+        public static void WriteWarning(string message)
+        {
+            if (!EventLog.SourceExists(Source))
+            {
+                EventLog.CreateEventSource(Source, sLog);
+            }
+
+            string machineName = "."; // this computer
+            using (EventLog log = new EventLog(sLog, machineName, Source))
+            {
+                log.WriteEntry(message, EventLogEntryType.Warning);
+            }
+        }
     }
 }
